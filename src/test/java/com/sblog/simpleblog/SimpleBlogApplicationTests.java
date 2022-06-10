2e4b0d700bc751eae0054fc6caf15d5924beb556
layue13 <layue13@protonmail.com>
2022-06-10 15:59:17 +0800

    Some test.

--- src/test/java/com/sblog/simpleblog/SimpleBlogApplicationTests.java
+++ src/test/java/com/sblog/simpleblog/SimpleBlogApplicationTests.java
@@ -1,13 +1,14 @@
 package com.sblog.simpleblog;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@SpringBootTest
 class SimpleBlogApplicationTests {
-
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleBlogApplicationTests.class);
     @Test
     void contextLoads() {
+        LOG.info("TEST load Logger");
     }
 
 }
